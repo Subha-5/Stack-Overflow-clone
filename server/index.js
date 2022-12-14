a8569e9ef96014670c736c80319be25fe4aa4385
Subha-5 <subhasadhu5@gmail.com>
2022-12-14 23:06:34 +0530

    build created

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
+const {CONNECTION_URL} = require('./config/keys')
 
 const app = express()
 dotenv.config()
@@ -21,9 +22,18 @@ app.use('/user', userRoutes)
 app.use('/questions', questionRoutes)
 app.use('/answer', answerRoutes)
 
+if(process.env.NODE_ENV=='production'){
+    const path = require('path')
+
+    app.get('/', (req, res) => {
+        app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 5000
 
-const DATABASE_URL = process.env.CONNECTION_URL
+const DATABASE_URL = CONNECTION_URL
 
 // mongoose.set('strictQuery', true);
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
